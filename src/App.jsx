109901b7d01f16e74de23c5903ed429c8cfac72d
ZHENK <chengzhenyang@gmail.com>
2020-10-22 14:50:58 +1300

    add question handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import { Button, Container, FormControl, FormHelperText, Input, InputLabel } from "@material-ui/core"
-import React, { useEffect } from "react"
+import { Button, Grid, FormControl, Input, InputLabel } from "@material-ui/core"
+import React, { useEffect, useState } from "react"
 import socketIOClient from "socket.io-client"
 const ENDPOINT = "http://127.0.0.1:3000"
 const socket = socketIOClient(ENDPOINT)
 
 
 function App() {
+    const [code, setCode] = useState("")
+    const [username, setUsername] = useState("zhenk")
+    const [users, setUsers] = useState({})
+
     useEffect(() => {
-        socket.on("createRoom", newRoomID => {
-            console.log(newRoomID)
+        socket.emit("username", {
+            username: username,
+        })
+
+        socket.on("users", users => {
+            setUsers(users)
+            console.log()
+        })
+
+        socket.on("createRoom", room => {
+            console.log(room)
+            setCode(room.code)
+        })
+
+        socket.on("joinRoom", room => {
+            console.log(room)
+        })
+
+        socket.on("question", data => {
+            console.log(data)
+        })
+
+        socket.on("end", data => {
+            console.log(data)
         })
 
         socket.on("error", data => {
@@ -16,31 +42,46 @@ function App() {
         })
     }, [])
 
+    useEffect(()=>{
+        socket.emit("username", {
+            username: username,
+        })
+    }, [username])
+
     const createRoom = () => {
         socket.emit("createRoom", {
-            host: "myname",
+            host: username,
             timer: 10,
-
+            international: false,
         })
     }
 
     const startGame = () => {
         socket.emit("startGame", {
-            username: "myname",
-            code: ""
+            code: code
+        })
+    }
+
+    const joinRoom = () => {
+        socket.emit("joinRoom", {
+            code: code
         })
     }
 
     return (
-        <Container>
+        <Grid container direction="column" justify="space-around" alignItems="center">
             <FormControl>
-                <InputLabel htmlFor="my-input">Email address</InputLabel>
-                <Input id="my-input" aria-describedby="my-helper-text" />
-                <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                <InputLabel htmlFor="my-input">Username</InputLabel>
+                <Input value={username} onChange={e => setUsername(e.target.value)}/>
             </FormControl>
-            <Button variant="contained" color="primary" primary onClick={createRoom}>createRoom</Button>
+            <FormControl>
+                <InputLabel htmlFor="my-input">Lobby Code</InputLabel>
+                <Input value={code} onChange={e => setCode(e.target.value)}/>
+            </FormControl>
+            <Button variant="contained" color="primary" onClick={createRoom}>createRoom</Button>
             <Button variant="contained" color="primary" onClick={startGame}>startGame</Button>
-        </Container>
+            <Button variant="contained" color="primary" onClick={joinRoom}>joinRoom</Button>
+        </Grid>
     )
 }
 
